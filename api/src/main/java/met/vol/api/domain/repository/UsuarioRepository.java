169244f80d346755e26ecbfa3f469b9b827e7029
Guigui-chan <guiportosilva2@gmail.com>
2026-07-17 03:25:57 -0300

    Implementando a interface UserDetailsService.

--- api/src/main/java/met/vol/api/domain/repository/UsuarioRepository.java
+++ api/src/main/java/met/vol/api/domain/repository/UsuarioRepository.java
@@ -2,6 +2,9 @@ package met.vol.api.domain.repository;
 
 import met.vol.api.domain.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findByLogin(String username);
 }
--- api/src/main/java/met/vol/api/infra/Identificador.java
+++ api/src/main/java/met/vol/api/infra/Identificador.java
@@ -1,6 +1,19 @@
 package met.vol.api.infra;
 
+import met.vol.api.domain.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-@Service public class Identificador {
+@Service public class Identificador implements UserDetailsService {
+
+    private UsuarioRepository BANCO;
+
+    public Identificador (UsuarioRepository banco) { BANCO = banco; }
+
+    @Override public UserDetails loadUserByUsername (String username) throws UsernameNotFoundException {
+
+        return BANCO.findByLogin(username);
+    }
 }
